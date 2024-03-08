d43915a8d0f90237118a176f98580beeaacb07f4
Swapna Dirisala <sdirisala@apple.com>
2024-03-07 17:18:15 -0800

    fetch API and async thunks

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,30 +1,38 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { connect } from "react-redux";
 import { removeTodo, markAsCompletedTodo } from "./actions";
-import { displayAlert } from "./thunks";
 import TodoListItem from "./TodoListItem";
+import { loadTodos } from "./thunks";
 import NewTodoForm from "./NewTodoForm";
 import './TodoList.css';
 
-const TodoList = ({todos = [], onRemovePressed, onCompletedPressed }) =>(
-    <div className="list-wrapper">
-        <NewTodoForm/>
-        {todos.map((todo, index)=> <TodoListItem 
-        key={index}
-        todo={todo} 
-        onRemovePressed={onRemovePressed}
-        onCompletedPressed={onCompletedPressed}
-        displayAlert={displayAlert}/>)}
-    </div>
-)
+const TodoList = ({todos = [], onRemovePressed, onCompletedPressed, isLoading, startLoadingTodos }) => {
+    useEffect(()=>{
+        startLoadingTodos()
+    }, [])
+    const loadingMessage = "Loading todos...";
+    const content = (
+        <div className="list-wrapper">
+                <NewTodoForm/>
+                {todos.map((todo, index)=> <TodoListItem 
+                key={index}
+                todo={todo} 
+                onRemovePressed={onRemovePressed}
+                onCompletedPressed={onCompletedPressed}/>)}
+        </div>);
+    return isLoading? loadingMessage : content
+}
 
 const mapStateToProps = state =>({
+    isLoading: state.isLoading,
     todos: state.todos,
 });
 
 const mapDispatchToProps = dispatch =>({
+    startLoadingTodos: () => dispatch(loadTodos()),
     onRemovePressed: text => dispatch(removeTodo(text)),
     onCompletedPressed: text => dispatch(markAsCompletedTodo(text))
+
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,4 +1,18 @@
+import { loadTodosInProgress, loadTodosSuccess, loadTodosFailure } from "./actions";
+
+export const loadTodos = () => async (dispatch, getState) =>{
+    try{
+        dispatch(loadTodosInProgress());
+        const response = await fetch('http://localhost:8080/todos-delay');
+        const todos = await response.json();
+        dispatch(loadTodosSuccess(todos));
+    } catch(error){
+        dispatch(loadTodosFailure());
+        dispatch(displayAlert(error));
+    }
+}
+
 export const displayAlert = text => () =>{
-    alert(`Clicked on: ${text}`)
-};
+    alert(text)
+}; 
 
